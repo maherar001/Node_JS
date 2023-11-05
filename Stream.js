65918439ef87ea3460532689f7d741ad8a8e2a83
maherar001 <maherar001@gmail.com>
2023-11-05 22:12:45 +0100

    Stream and Piping

--- Stream.js
+++ Stream.js
@@ -1,2 +1,15 @@
 const fs = require('fs');
 
+const readStream = fs.createReadStream('./Doc/blog3.txt', {encoding: 'utf8'});
+const writeStream = fs.createWriteStream('./doc/blog4.txt');
+
+// readStream.on('data', (chunk) =>
+// {
+//     console.log("--------New chunk----------");
+//     console.log(chunk);
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+// });
+
+//piping
+readStream.pipe(writeStream);
