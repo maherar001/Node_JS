3b289f46264af47be30dbafdd7e444fcf7322c0e
maherar001 <maherar001@gmail.com>
2023-11-09 21:27:27 +0100

    server requests and responses

--- server.js
+++ server.js
@@ -1,7 +1,46 @@
 const http = require("http");
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //paths
+
+    let path = './views/';
+    switch(req.url){
+        case '/':
+        case '/home':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            path += 'about.html';
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    //set header content type
+    res.setHeader("Content-Type", "text/html");
+
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        }
+        else{
+            res.end(data);
+        }
+    })
 
     });
 
