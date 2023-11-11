1afda8c3c7d4aa6a01d7843d4ec1c7b21fdacc82
maherar001 <maherar001@gmail.com>
2023-11-11 23:00:47 +0100

    ehs, middleware with morgan

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
 
-// listen for request
+//register view engine
+app.set('view engine', 'ejs');
 
+// listen for request
 app.listen(3000);
 
+//middleware & static fiels
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
 // Home Page
-app.get('/home',(req, res) =>
+app.get('/',(req, res) =>
 {
-    res.sendFile('./views/index.html', {root: __dirname});
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index', {title: 'Home' , blogs});
 } );
 
 // About
 app.get('/about',(req, res) =>
 {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
 } );
 
-//redirects
-app.get('/about-us',(req, res) =>
-{
-    res.redirect('/about');
-} );
+app.get("/blogs/create", (req, res) =>{
+    res.render('create', {title: 'New Blog'});
+})
 
 // 404 not found
 app.use((req, res)=>{
-res.status(404).sendFile('./views/404.html', {root: __dirname});
+res.status(404).render('404', {title: 'Error 404'}); 
 });
