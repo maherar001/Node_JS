f7c249a6309b5abaebc46c9eefc4025d4b2a7948
maherar001 <maherar001@gmail.com>
2023-11-17 21:28:10 +0100

    get, post, and delete requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongooes = require('mongoose');
 const Project = require('./models/portfolio');
+const { result } = require('lodash');
 
 // password F2qPF0ONT9Od9w62
 
@@ -19,44 +20,89 @@ app.set('view engine', 'ejs');
 
 //middleware & static fiels
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // mongoose and mongo sandbox routes 
-app.get('/add-project', (req, res) =>{
-    const project = new Project({
-        title: 'My Portfolio',
-        description: 'This is my first project in Node JS.',
-        project_url: 'https://www.google.com/',
-        gitRepository: 'https://github.com/maherar001/Node_JS'
-    });
+// app.get('/add-project', (req, res) =>{
+//     const project = new Project({
+//         title: 'My Portfolio',
+//         description: 'This is my first project in Node JS.',
+//         project_url: 'https://www.google.com/',
+//         gitRepository: 'https://github.com/maherar001/Node_JS'
+//     });
 
-    project.save()
-    .then ((result) => {
-        res.send(result)
-    })
-    .catch((err) =>{
-        console.log("Error is = " + err);
-    });
-})
+//     project.save()
+//     .then ((result) => {
+//         res.send(result)
+//     })
+//     .catch((err) =>{
+//         console.log("Error is = " + err);
+//     });
+// })
 
 
 // Home Page
 app.get('/',(req, res) =>
 {
-    const blogs = [
-        {title: 'Yoshi finds eggs', descr: 'Lorem ipsum dolor sit amet consectetur' , project_url: 'https://www.google.com/'},
-        {title: 'Mario finds stars', descr: 'Lorem ipsum dolor sit amet consectetur', project_url: 'https://stackoverflow.com/'},
-        {title: 'How to defeat bowser', descr: 'Lorem ipsum dolor sit amet consectetur', project_url: 'https://www.youtube.com/'},
-      ];
-    res.render('index', {title: 'Home' , blogs});
+    res.redirect('/projects');
 } );
+
+// All Projects
+app.get('/projects', (req, res) =>{
+    Project.find().sort({ createdAt: -1})
+    .then((result) =>{
+        res.render('index', {title: 'All Projects', projects: result})
+    })
+    .catch((err) =>{
+        console.log(err);
+    })
+})
 
 // About
 app.get('/about',(req, res) =>
 {
     res.render('about', {title: 'About'});
 } );
+
+app.post('/projects', (req, res) => {
+    const project = new Project(req.body);
+
+    
+    console.log(req.body);
+
+    project.save()
+    .then ((result) =>  {
+        res.redirect('/projects');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+});
+
+app.get('/projects/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    Project.findById(id)
+    .then((result) => {
+        res.render('details', {projects: result, title: "Project Details" });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.delete('/projects/:id', (req, res) => {
+    const id = req.params.id;
+
+    Project.findByIdAndDelete(id)
+    .then(result => {
+        res.json({redirect: '/projects'})
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+})
 
 app.get("/blogs/create", (req, res) =>{
     res.render('create', {title: 'New Blog'});
